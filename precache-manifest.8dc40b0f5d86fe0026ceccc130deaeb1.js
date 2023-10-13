8f12c761c8bf85a83eaf9521d3065283984b826a
Gabszy24 <corderogabrielle@gmail.com>
2023-10-13 13:23:58 +0800

    d

---
+++ precache-manifest.8dc40b0f5d86fe0026ceccc130deaeb1.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "82580f9fdc76b12581b4",
+    "url": "/static/css/main.9eaf3c7b.chunk.css"
+  },
+  {
+    "revision": "82580f9fdc76b12581b4",
+    "url": "/static/js/main.48b4eaa8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "de65e04303652b46a084",
+    "url": "/static/css/2.6d14e02a.chunk.css"
+  },
+  {
+    "revision": "de65e04303652b46a084",
+    "url": "/static/js/2.bac60901.chunk.js"
+  },
+  {
+    "revision": "d30c9e30978c9278180dcce2bb66c054",
+    "url": "/static/media/sidebar-2.d30c9e30.jpg"
+  },
+  {
+    "revision": "61c4c50e61d68de31c8951af5470a2c2",
+    "url": "/static/media/sidebar-1.61c4c50e.jpg"
+  },
+  {
+    "revision": "c963330bcd48670a75ffdd38622b1532",
+    "url": "/static/media/sidebar-3.c963330b.jpg"
+  },
+  {
+    "revision": "f34e943fbffb73c68e761828b1c00980",
+    "url": "/static/media/sidebar-4.f34e943f.jpg"
+  },
+  {
+    "revision": "365e014719904200a220834f603497cc",
+    "url": "/static/media/admnpic2.365e0147.png"
+  },
+  {
+    "revision": "5fbe38c6c0a349ff0f38ec04f28806a8",
+    "url": "/static/media/smokey_bg.5fbe38c6.jpg"
+  },
+  {
+    "revision": "140de3bce35ed1b1010fbca711d9b97f",
+    "url": "/static/media/white.140de3bc.png"
+  },
+  {
+    "revision": "a80cd7a77206b15ee13a582ea53b4cab",
+    "url": "/static/media/logo-white.a80cd7a7.svg"
+  },
+  {
+    "revision": "49b54380ba092ba68ab160f3dd8d5773",
+    "url": "/static/media/register.49b54380.jpeg"
+  },
+  {
+    "revision": "59fc83e26c663ade5dcae6e7d56ca970",
+    "url": "/static/media/login.59fc83e2.jpeg"
+  },
+  {
+    "revision": "35f7d126c07a40a893f3f6cc8d1c3717",
+    "url": "/static/media/lock.35f7d126.jpeg"
+  },
+  {
+    "revision": "5b7c1cad66a2efdbf3604615cc9d7d1c",
+    "url": "/static/media/clint-mckoy.5b7c1cad.jpg"
+  },
+  {
+    "revision": "4d2f2f4baa617ba08685b4d9118dc029",
+    "url": "/static/media/bg-pricing.4d2f2f4b.jpeg"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "3904270e406e0cb84e14b4684dcf8be2",
+    "url": "/index.html"
+  }
+];
